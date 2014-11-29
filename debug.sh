5c5712026d478600ea257b02538761b1a2088df9
meruelo <meruelo@gmail.com>
2014-11-29 18:39:44 +0100

    + Stop execution if *.elf is not found + Removed path for 'arm-none-eabi-gdb'

--- debug.sh
+++ debug.sh
@@ -10,12 +10,18 @@ if [ -f "$1" ]; then
 	else
 		path="$(echo "$1" | sed 's/\(.*\)\/\(kernel.img\)/\1/')"
 		path="$path/build/output.elf"
+
+		if [!-f $path ]; then
+			echo 'Elf file not found'
+			exit 1
+		fi
 	fi
-	./arm-2008q3/bin/arm-none-eabi-gdb -ex "file $path" -ex "target remote :1234"
+	arm-none-eabi-gdb -ex "file $path" -ex "target remote :1234"
 
 	#3rd: End
 	$(ps | grep qemu | sed 's/\([0-9]\{1,5\}\).*/\1/' | xargs kill -s SIGTERM )
 
 else
 	echo 'Image not found'
+	exit 1
 fi
